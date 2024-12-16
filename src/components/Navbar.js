5a6b83da37e4c46163c7e13519c69e2ab0b5041d
chardgrey <cardosarichard@gmail.com>
2024-12-16 15:30:58 +0800

    Update Navbar component to link to downloadable resume

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faDownload } from '@fortawesome/free-solid-svg-icons'
 
+
 function Navbar({home, projects, expertise, contact}) {
   return (
     <div className="fixed top-0 w-full z-50 bg-gr3y">
@@ -12,7 +13,7 @@ function Navbar({home, projects, expertise, contact}) {
             <a href={expertise} className="px-4 py-2 relative after:content-[''] after:absolute after:w-full after:scale-x-0 after:h-0.5 after:bottom-0 after:left-0 after:bg-blu3 after:transition-transform after:duration-300 hover:after:scale-x-100 hover:text-gray-400 transition-colors duration-300">Expertise</a>
             <a href={projects} className="px-4 py-2 relative after:content-[''] after:absolute after:w-full after:scale-x-0 after:h-0.5 after:bottom-0 after:left-0 after:bg-blu3 after:transition-transform after:duration-300 hover:after:scale-x-100 hover:text-gray-400 transition-colors duration-300">Projects</a>
             <a href={contact} className="px-4 py-2 relative after:content-[''] after:absolute after:w-full after:scale-x-0 after:h-0.5 after:bottom-0 after:left-0 after:bg-blu3 after:transition-transform after:duration-300 hover:after:scale-x-100 hover:text-gray-400 transition-colors duration-300">Contact</a>
-            <a href="#" className="px-4 py-2 relative after:content-[''] after:absolute after:w-full after:scale-x-0 after:h-0.5 after:bottom-0 after:left-0 after:bg-blu3 after:transition-transform after:duration-300 hover:after:scale-x-100 hover:text-gray-400 transition-colors duration-300">Resume <FontAwesomeIcon icon={faDownload} className='text-base px-2' /></a>
+            <a href="/CardosaRichard CV.pdf" download className="px-4 py-2 relative after:content-[''] after:absolute after:w-full after:scale-x-0 after:h-0.5 after:bottom-0 after:left-0 after:bg-blu3 after:transition-transform after:duration-300 hover:after:scale-x-100 hover:text-gray-400 transition-colors duration-300">Resume <FontAwesomeIcon icon={faDownload} className='text-base pr-2'/></a>
           </div>
         </div>
       </nav>
